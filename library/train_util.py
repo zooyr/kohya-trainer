af92dbd51ca62fd8a21a6c47dde9f6e7004bcb59
Your Name <you@example.com>
2023-03-21 15:50:08 +0800

    i

--- library/train_util.py
+++ library/train_util.py
@@ -46,6 +46,8 @@ import safetensors.torch
 
 import library.model_util as model_util
 
+from icecream import ic
+
 # Tokenizer: checkpointから読み込むのではなくあらかじめ提供されているものを使う
 TOKENIZER_PATH = "openai/clip-vit-large-patch14"
 V2_STABLE_DIFFUSION_PATH = "stabilityai/stable-diffusion-2"     # ここからtokenizerだけ使う v2とv2.1はtokenizer仕様は同じ
